cdf07f8ceca1c76685e28dd94673aa50835f58b9
kelopakyakan <155567976+kelopakyakan@users.noreply.github.com>
2024-01-06 05:00:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ENV 8080 python3 clever.py & python fetch.py & python start.py
+
+# Set environment variable
+export PORT=8080
+
+# Run the Python scripts in the background
+python3 clever.py &
+python3 fetch.py &
+python3 start.py

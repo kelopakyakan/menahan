ef6082b0266f1e2376589b8b43aec2f5f1454be0
kelopakyakan <155567976+kelopakyakan@users.noreply.github.com>
2024-01-06 05:22:45 +0700

    Update clever.py

--- clever.py
+++ clever.py
@@ -1,4 +1,4 @@
-import os  # Don't forget to import the os module
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +8,9 @@ def hello():
     return 'Hello, Clever Cloud!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # Menggunakan nilai default 8080 jika PORT tidak ditemukan di variabel lingkungan
+    port = int(os.environ.get('PORT', 8080))  # versi sebelumnya
+    # Menggunakan nilai default 8080 jika PORT tidak ditemukan di variabel lingkungan
+    port = int(os.environ['PORT']) if 'PORT' in os.environ else 8080  # versi tanpa os.environ.get
+
+    app.run(debug=True, host='0.0.0.0', port=port)
